a8c85423ea081f976654a1f9fbb30664cca72dba
DuongDat <duongvandat080602@gmail.com>
2024-03-04 17:33:26 +0700

    Home Page - P4

--- frontend/src/components/Testimonial/Testimonial.jsx
+++ frontend/src/components/Testimonial/Testimonial.jsx
@@ -6,7 +6,8 @@ import { Autoplay, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 
-// import required modules
+import ava01 from "../../assets/img/ava-3.jpg";
+import "./Testimonial.css";
 
 function Testimonial() {
   return (
@@ -17,21 +18,86 @@ function Testimonial() {
         clickable: true,
       }}
       modules={[Autoplay, Pagination]}
-      className="mySwiper"
       autoplay={{
-        delay: 1500,
+        delay: 2000,
         disableOnInteraction: false,
       }}
+      breakpoints={{
+        320: {
+          slidesPerView: 1,
+        },
+        640: {
+          slidesPerView: 1,
+        },
+        768: {
+          slidesPerView: 2,
+        },
+        1024: {
+          slidesPerView: 3,
+        },
+      }}
     >
-      <SwiperSlide>Slide 1</SwiperSlide>
-      <SwiperSlide>Slide 2</SwiperSlide>
-      <SwiperSlide>Slide 3</SwiperSlide>
-      <SwiperSlide>Slide 4</SwiperSlide>
-      <SwiperSlide>Slide 5</SwiperSlide>
-      <SwiperSlide>Slide 6</SwiperSlide>
-      <SwiperSlide>Slide 7</SwiperSlide>
-      <SwiperSlide>Slide 8</SwiperSlide>
-      <SwiperSlide>Slide 9</SwiperSlide>
+      <SwiperSlide>
+        <div className="testimonial py-4 px-3">
+          <div className="swiper-client-msg">
+            <p>Tour du lịch Mù Cang chải thật tuyệt vời tôi rất thích nó!!!</p>
+          </div>
+          <div className="swiper-client-data d-flex justify-content-center align-items-center gap-4 mt-3">
+            <img src={ava01} alt="" />
+            <div className="client-data-details">
+              <h6 className="mb-0 mt-3">User 1</h6>
+              <p>User 1</p>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="testimonial py-4 px-3">
+          <div className="swiper-client-msg">
+            <p>Tour du lịch Mù Cang chải thật tuyệt vời tôi rất thích nó!!!</p>
+          </div>
+          <div className="swiper-client-data d-flex justify-content-center align-items-center gap-4 mt-3">
+            <img src={ava01} alt="" />
+            <div className="client-data-details">
+              <h6 className="mb-0 mt-3">User 1</h6>
+              <p>User 1</p>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="testimonial py-4 px-3">
+          <div className="swiper-client-msg">
+            <p>Tour du lịch Mù Cang chải thật tuyệt vời tôi rất thích nó!!!</p>
+          </div>
+          <div className="swiper-client-data d-flex justify-content-center align-items-center gap-4 mt-3">
+            <img src={ava01} alt="" />
+            <div className="client-data-details">
+              <h6 className="mb-0 mt-3">User 1</h6>
+              <p>User 1</p>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
+      <SwiperSlide>
+        <div className="testimonial py-4 px-3">
+          <div className="swiper-client-msg">
+            <p>
+              Tour du lịch Mù Cang chải thật tuyệt!!! Tour du lịch Mù Cang chải
+              thật!!! Tour du lịch Mù Cang chải thật!!!Tour du lịch Mù Cang chải
+              thật!!! Tour du lịch Mù Cang chải thật!!! Tour du lịch Mù Cang
+              chải thật!!!
+            </p>
+          </div>
+          <div className="swiper-client-data d-flex justify-content-center align-items-center gap-4 mt-3">
+            <img src={ava01} alt="" />
+            <div className="client-data-details">
+              <h6 className="mb-0 mt-3">User 1</h6>
+              <p>User 1</p>
+            </div>
+          </div>
+        </div>
+      </SwiperSlide>
     </Swiper>
   );
 }
