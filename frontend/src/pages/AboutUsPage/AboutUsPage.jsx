4b5549de24cb119b0916574684cd62da35dffe0c
DuongDat <duongvandat080602@gmail.com>
2024-04-15 21:49:51 +0700

    About Page -P5

--- frontend/src/pages/AboutUsPage/AboutUsPage.jsx
+++ frontend/src/pages/AboutUsPage/AboutUsPage.jsx
@@ -2,6 +2,7 @@ import { Typewriter } from "react-simple-typewriter";
 
 import AboutGif from "../../assets/img/about.gif";
 import CurveImg from "../../assets/img/curve.png";
+import AboutSocail from "../../assets/img/about-travel.png";
 import "./AboutUsPage.css";
 import Subtitle from "../../shared/Subtitle";
 
@@ -69,7 +70,7 @@ function AboutUsPage() {
           <div className="text-end">
             <div className="d-flex">
               <div className="flex-grow-1"></div>
-              <Subtitle subtitle="About us!" />
+              <Subtitle subtitle="Service!" />
             </div>
             <h4 className="header__title">
               Khai mở những ký ức mới cùng
@@ -119,7 +120,7 @@ function AboutUsPage() {
                   <i className="ri-hand-coin-fill"></i>
                 </div>
                 <div className="content">
-                  <h2>Chăm sóc khách hành</h2>
+                  <h2>Chăm sóc khách hàng</h2>
                   <p>
                     Tại <strong>HoYoViVu</strong>, chúng tôi luôn đặt lợi ích và
                     sự thoải mái của khách hàng lên hàng đầu. Với đội ngũ nhân
@@ -145,6 +146,186 @@ function AboutUsPage() {
                     phá thế giới một cách tiết kiệm và thú vị hơn bao giờ hết!
                   </p>
                 </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section-bg pb-5">
+        <div className="container">
+          <div className="text-start">
+            <Subtitle subtitle="Our social!" />
+            <h4 className="header__title">
+              Cập nhật thông tin với trang thông tin.
+              <br /> HoYoViVu
+            </h4>
+          </div>
+          <div className="row align-items-center">
+            <div className="col-lg-6 col-md-6 col-12">
+              <p className="text-about">
+                Hãy tham gia cùng chúng tôi xây dựng mạng xã hội để không bỏ lỡ
+                bất kỳ thông tin hay cập nhật nào về các hành trình phiêu lưu
+                độc đáo và đầy cảm hứng! Tại <strong>HoYoViVu</strong>, chúng
+                tôi không chỉ là một trang web du lịch, mà còn là một cộng đồng
+                đam mê khám phá và chia sẻ. Theo dõi chúng tôi trên các nền tảng
+                xã hội dưới đây để tham gia vào cuộc trò chuyện, chia sẻ hình
+                ảnh, và khám phá thêm về những nơi thú vị trên thế giới!
+              </p>
+              <div className="wrapper-social">
+                <div className="socail-btn">
+                  <div className="socail-icon">
+                    <i className="ri-facebook-circle-fill"></i>
+                  </div>
+                  <span>Facebook</span>
+                </div>
+                <div className="socail-btn">
+                  <div className="socail-icon">
+                    <i className="ri-instagram-fill"></i>
+                  </div>
+                  <span>Instagram</span>
+                </div>
+                <div className="socail-btn">
+                  <div className="socail-icon">
+                    <i className="ri-twitter-fill"></i>
+                  </div>
+                  <span>Twitter</span>
+                </div>
+                <div className="socail-btn">
+                  <div className="socail-icon">
+                    <i className="ri-youtube-fill"></i>
+                  </div>
+                  <span>Youtube</span>
+                </div>
+                <div className="socail-btn">
+                  <div className="socail-icon">
+                    <i className="ri-tiktok-fill"></i>
+                  </div>
+                  <span>TikTok</span>
+                </div>
+              </div>
+            </div>
+            <div className="col-lg-6 col-md-6 col-12">
+              <div className="d-flex justify-content-center">
+                <img
+                  src={AboutSocail}
+                  alt="social image"
+                  className="image-social"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section className="section-bg pt-5 pb-5">
+        <div className="text-center mb-5">
+          <div className="d-flex justify-content-center mb-2">
+            <Subtitle subtitle="Community Support" />
+          </div>
+          <h4 className="header__title">
+            Đồng hành và hỗ trợ bạn mỗi bước trên hành trình khám phá của mình.
+          </h4>
+        </div>
+        <div className="container container-medium">
+          <div className="list-avatars">
+            <img
+              className="avatar"
+              src="https://th.bing.com/th/id/OIP.XWua3DEYQ4JUm4RNAmMFlQHaFw?w=252&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+              alt=""
+            />
+            <img
+              className="avatar"
+              src="https://th.bing.com/th/id/OIP.XWua3DEYQ4JUm4RNAmMFlQHaFw?w=252&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+              alt=""
+            />
+            <img
+              className="avatar"
+              src="https://th.bing.com/th/id/OIP.XWua3DEYQ4JUm4RNAmMFlQHaFw?w=252&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+              alt=""
+            />
+            <img
+              className="avatar"
+              src="https://th.bing.com/th/id/OIP.XWua3DEYQ4JUm4RNAmMFlQHaFw?w=252&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+              alt=""
+            />
+            <img
+              className="avatar"
+              src="https://th.bing.com/th/id/OIP.XWua3DEYQ4JUm4RNAmMFlQHaFw?w=252&h=196&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+              alt=""
+            />
+          </div>
+        </div>
+      </section>
+      <section className="section-bg">
+        <div className="text-center mb-5">
+          <div className="d-flex justify-content-center mb-2">
+            <Subtitle subtitle="Give us feedback" />
+          </div>
+          <h4 className="header__title">
+            Phản hồi đánh giá của bạn đối với chúng tôi. <br />
+            HoYoViVu
+          </h4>
+        </div>
+        <div className="container container-medium">
+          <div className="tour-content bg-transparent">
+            <div className="form-item-container pt-5 pb-2">
+              <div className="mb-4">
+                <label htmlFor="title" className="form-label">
+                  Họ tên (<span className="text-red">*</span>):
+                </label>
+                <input
+                  type="text"
+                  id="title"
+                  name="title"
+                  className="form-control"
+                  placeholder="Họ và tên"
+                />
+              </div>
+              <div className="mb-4">
+                <div className="row">
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <label htmlFor="country" className="form-label">
+                      Email (<span className="text-red">*</span>):
+                    </label>
+                    <input
+                      type="text"
+                      id="title"
+                      name="title"
+                      className="form-control"
+                      placeholder="Email của bạn"
+                    />
+                  </div>
+                  <div className="col-lg-6 col-md-6 col-12">
+                    <label htmlFor="country" className="form-label">
+                      Số điện thoại (<span className="text-red">*</span>):
+                    </label>
+                    <input
+                      type="text"
+                      id="title"
+                      name="title"
+                      className="form-control"
+                      placeholder="Số điện thoại của bạn"
+                    />
+                  </div>
+                </div>
+              </div>
+              <div className="mb-4">
+                <label htmlFor="country" className="form-label">
+                  Nội dung (<span className="text-red">*</span>):
+                </label>
+                <div className="form-floating">
+                  <textarea
+                    className="form-control"
+                    placeholder="Leave a comment here"
+                    id="floatingTextarea"
+                    style={{ height: 200 }}
+                  ></textarea>
+                  <label htmlFor="floatingTextarea">Nội dung phản hồi</label>
+                </div>
+              </div>
+              <div className="my-5 w-100 d-flex justify-content-center align-items-center flex-wrap gap-3">
+                <button type="reset" className="button btn-submit">
+                  Phản hồi
+                </button>
               </div>
             </div>
           </div>
